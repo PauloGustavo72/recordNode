14f8bf4d6ffc09076612aeaca69d889e7bc5058b
PauloGustavo72 <paulogustavo777@yahoo.com.br>
2018-01-10 22:31:17 -0200

    banco configurado

--- recordNodejs/bancoDeDados/dbConnection.js
+++ recordNodejs/bancoDeDados/dbConnection.js
@@ -1,11 +1,15 @@
  var mysql  = require('mysql');
 
  function createDBConnection(){
+   	var url = process.env.CLEARDB_DATABASE_URL;
+   	var grupos = url.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true);
+
+
     return mysql.createConnection({
-                host:'localhost',
-                user:'root',
-                password:'root',
-                database:'record_english'
+                host:grupos[3],
+                user:grupos[1],
+                password:grupos[2],
+                database:grupos[4]
           });
 
 }
